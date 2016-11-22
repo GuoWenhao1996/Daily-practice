8f25ae2ef0e8a041a6b9b34054c9dfd899664357
GuoWenhao1996 <1842297753@qq.com>
2016-11-22 23:07:37 +0800

    邮件发送更新

--- My_Email/app/src/main/java/com/example/guowh/my_email/MainActivity.java
+++ My_Email/app/src/main/java/com/example/guowh/my_email/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.guowh.my_email;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button Button_Login = (Button) findViewById(R.id.button_login);//获取登录按钮资源
+        final EditText EditText_address = (EditText) findViewById(R.id.editText_Address);
+        final EditText EditText_pwd = (EditText) findViewById(R.id.editText_Password);
+
+        Button_Login.setOnClickListener(new View.OnClickListener() {//创建监听
+            @Override
+            public void onClick(View view) {
+                if (Loginin()) {
+                    Toast.makeText(getApplicationContext(), "登录成功！", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Send_Email.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private boolean Loginin() {
+        try {
+            return true;
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "登录失败！", Toast.LENGTH_SHORT).show();
+            return false;
+        }
     }
 }
---
+++ My_Email/app/src/main/java/com/example/guowh/my_email/Send_Email.java
@@ -0,0 +1,16 @@
+package com.example.guowh.my_email;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by guowh on 2016/11/22.
+ */
+
+public class Send_Email extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.send_email);
+    }
+}
