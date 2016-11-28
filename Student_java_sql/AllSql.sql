6e2c67615d322a2a7cc69dbaa14f31023136cd2b
GuoWenhao1996 <1842297753@qq.com>
2016-11-28 23:54:33 +0800

    进一步提交数据库信息

--- Student_java_sql/AllSql.sql
+++ Student_java_sql/AllSql.sql
@@ -18,7 +18,7 @@ use Link_DB_Student_631406010109
 --insert into xx values('004','重庆师范大学') 
 --insert into xx values('005','重庆医科大学') 
 --insert into xx values('006','重庆邮电大学')
-select * from xx 
+--select * from xx 
 
 ----向学院表插值
 --insert into xy values('11','001','信息科学与工程学院')
@@ -27,7 +27,7 @@ select * from xx
 --insert into xy values('21','002','重大的一个学院')
 --insert into xy values('31','003','工商的一个学院')
 --insert into xy values('41','004','重师的一个学院')
-select xydm,xymc,xxmc from xy a,xx b where a.xxdm=b.xxdm
+--select xydm,xymc,xxmc from xy a,xx b where a.xxdm=b.xxdm
 
 ----向专业表插值
 --insert into zy values('11','11','计算机科学与技术')
@@ -36,7 +36,7 @@ select xydm,xymc,xxmc from xy a,xx b where a.xxdm=b.xxdm
 --insert into zy values('14','11','物联网')
 --insert into zy values('21','12','土木造价专业')
 --insert into zy values('31','13','齿轮设计专业')
-select zydm,zymc,xymc from zy a,xy b where a.xydm=b.xydm
+--select zydm,zymc,xymc from zy a,xy b where a.xydm=b.xydm
 
 ----向班级表插值
 --insert into bj values('11','11','计科1401班')
@@ -52,21 +52,21 @@ select zydm,zymc,xymc from zy a,xy b where a.xydm=b.xydm
 --insert into bj values('41','14','物联1401班')
 --insert into bj values('42','14','物联1402班')
 --insert into bj values('43','14','物联1403班')
-select bjdm,bjmc,zymc from bj a,zy b where a.zydm=b.zydm
+--select bjdm,bjmc,zymc from bj a,zy b where a.zydm=b.zydm
 
 ----向政治面貌表插值
 --insert into zzmm values('01','共产党员')
 --insert into zzmm values('02','共青团员')
 --insert into zzmm values('03','群众')
 --insert into zzmm values('04','其他')
-select * from zzmm
+--select * from zzmm
 
 ----向民族表插值
 --insert into mz values('01','汉族')
 --insert into mz values('02','藏族')
 --insert into mz values('03','回族')
 --insert into mz values('04','其它民族')
-select * from mz
+--select * from mz
 
 ----向亲属称谓表插值
 --insert into qscw values('01','爸爸')
@@ -75,7 +75,7 @@ select * from mz
 --insert into qscw values('04','哥哥')
 --insert into qscw values('05','娘娘')
 --insert into qscw values('06','姨夫')
-select * from qscw
+--select * from qscw
 
 ----向教师表插值
 --insert into js values('T000000001','王家伟')
@@ -85,7 +85,7 @@ select * from qscw
 --insert into js values('T000000005','娄路')
 --insert into js values('T000000006','许强')
 --insert into js values('T000000007','蒋伟')
-select * from js
+--select * from js
 
 ----向课程表插值
 --insert into kc values('C000000001','数据库原理B',3.5)
@@ -96,8 +96,45 @@ select * from js
 --insert into kc values('C000000006','嵌入式程序设计（WMARM）',2.5)
 --insert into kc values('C000000007','数据库及应用课程设计',2)
 --insert into kc values('C000000008','数学建模',2.5)
-select * from kc
+--select * from kc
 
 ----向上课安排表插值
 --insert into skap values('C000000001','T000000001','周二上午3-4节&&周四上午1-2节','20406&&20406')
-select * from skap
+--insert into skap values('C000000002','T000000002','周一下午7-8节&&周二下午5-6节','20203&&20423')
+--insert into skap values('C000000003','T000000003','周三下午5-6节&&周五上午3-4节','20312&&20312')
+--insert into skap values('C000000004','T000000004','周二下午7-8节','20510')
+--insert into skap values('C000000005','T000000005','周五上午1-2节','20505')
+--insert into skap values('C000000006','T000000006','周一上午1-2节&&周三上午3-4节','20505&&20505')
+--insert into skap values('C000000007','T000000001','暂定','暂定')
+--insert into skap values('C000000008','T000000007','周三下午7-8节&&周五下午7-8节','20505&&20505')
+--select kcmc,jsxm,sksj,skjs from skap a,kc b,js c where a.kcdm=b.kcdm and a.gh=c.gh
+
+----向学生表插值
+--insert into xs values('631406010109','01','02','11','郭文浩','男','20','1996-01-01','山西省某市某县某镇某小区')
+--insert into xs values('631406010102','01','02','11','莫天金','男','19','1997-01-01','重庆市某县某镇某小区')
+--insert into xs values('631406010103','01','02','11','吴国平','男','20','1996-01-01','贵州省某市某县某镇某小区')
+--insert into xs values('631406010124','01','02','11','王增辉','男','20','1996-01-01','江苏省某市某县某镇某小区')
+--insert into xs values('631406010127','02','02','11','次拉姆','女','20','1996-01-01','西藏自治区某市某县某镇某小区')
+--insert into xs values('631406010115','01','03','11','肖洒益','男','20','1996-01-01','重庆市某县某镇某小区')
+--insert into xs values('631406010201','01','02','12','肖霞','女','20','1996-01-01','重庆市某县某镇某小区')
+--insert into xs values('631406010401','01','02','14','刘翠芳','女','20','1996-01-01','山西省某市某县某镇某小区')
+--select xh,xsxm,bjmc,xb,nl,csrq,mzmc,zzmmmc,jtzz from xs a,mz b,zzmm c,bj d where a.mzdm=b.mzdm and a.zzmmdm=c.zzmmdm and a.bjdm=d.bjdm
+--select xh,xsxm,xxmc,xymc,zymc,bjmc,xb,nl,csrq,mzmc,zzmmmc,jtzz 
+--	from xs a,mz b,zzmm c,bj d,xx e,xy f,zy g 
+--	where a.mzdm=b.mzdm and a.zzmmdm=c.zzmmdm and a.bjdm=d.bjdm and e.xxdm=f.xxdm and f.xydm=g.xydm and g.zydm=d.zydm
+	
+----向社会关系表插值
+--insert into shgx values('01','631406010109','郭某某','139********')
+--insert into shgx values('02','631406010109','任某某','139********')
+--insert into shgx values('01','631406010102','莫某某','185********')
+--insert into shgx values('02','631406010102','某某某','187********')
+--insert into shgx values('01','631406010124','王某某','137********')
+--insert into shgx values('02','631406010124','某某某','184********')
+--insert into shgx values('01','631406010401','刘某某','130********')
+--insert into shgx values('02','631406010401','某某某','131********')
+--select * from shgx
+--select xsxm,chmc,qsxm,qslxfs from xs a,qscw b,shgx c where a.xh=c.xh and b.chdm=c.chdm
+
+----向惩罚管理表插值
+----向奖励管理表插值
+----向选课表插值
