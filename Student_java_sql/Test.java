c31b4aaa0ea5487cd2e1b7e1b6176ea24b889b6b
GuoWenhao1996 <1842297753@qq.com>
2016-11-30 21:20:18 +0800

    提交学生查询

--- Student_java_sql/Test.java
+++ Student_java_sql/Test.java
@@ -9,7 +9,6 @@ import java.sql.Statement;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author guowh
@@ -22,27 +21,35 @@ public class Test {
         Statement dbState = null;
         ResultSet dbRs = null;
         String sql = null;
-        DBHelper help = new DBHelper();
-        help.TestConn();
+        DBHelper dbhelpr = new DBHelper();
+        dbhelpr.TestConn();
 
         //查询student表
         try {
             System.out.println("\n查询学生信息管理表表中内容：");
-            dbConn = help.GetConnection();
+            dbConn = dbhelpr.GetConnection();
             dbState = dbConn.createStatement();
-            sql = "select * from 学生信息管理表";
+            sql = "select xh,xsxm,xxmc,xymc,zymc,bjmc,xb,nl,csrq,mzmc,zzmmmc,jtzz \n"
+                    + "	from xs a,mz b,zzmm c,bj d,xx e,xy f,zy g \n"
+                    + "	where a.mzdm=b.mzdm and a.zzmmdm=c.zzmmdm and a.bjdm=d.bjdm and e.xxdm=f.xxdm and f.xydm=g.xydm and g.zydm=d.zydm\n";
             dbRs = dbState.executeQuery(sql);
+                            System.out.println("学号：\t\t姓名：\t学校：\t\t学院：\t\t\t专业：\t\t\t班级：\t\t性别：\t出生日期：\t民族：\t政治面貌：\t家庭住址：" );
             while (dbRs.next()) {
-                System.out.print("学号：" + dbRs.getString("学号"));
-                System.out.print("\t姓名：" + dbRs.getString("姓名"));
-                System.out.print("\t性别：" + dbRs.getString("性别"));
-                System.out.println("\t出生日期：" + dbRs.getString("出生日期"));
-                System.out.println("\t学生简历：" + dbRs.getString("出生日期"));
-                System.out.println("\t ：" + dbRs.getString("出生日期"));
+                System.out.print("" + dbRs.getString("xh"));
+                System.out.print("\t" + dbRs.getString("xsxm"));
+                System.out.print("\t" + dbRs.getString("xxmc"));
+                System.out.print("\t" + dbRs.getString("xymc"));
+                System.out.print("\t" + dbRs.getString("zymc"));
+                System.out.print("\t" + dbRs.getString("bjmc"));
+                System.out.print("\t" + dbRs.getString("xb"));
+                System.out.print("\t" + dbRs.getString("csrq"));
+                System.out.print("\t" + dbRs.getString("mzmc"));
+                System.out.print("\t" + dbRs.getString("zzmmmc"));
+                System.out.println("\t" + dbRs.getString("jtzz"));
             }
             dbRs.close();
             dbState.close();
-            help.Close();
+            dbhelpr.Close();
         } catch (SQLException ex) {
             System.err.println(ex.getMessage());
         }
