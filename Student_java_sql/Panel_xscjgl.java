9f9944b26756044b0a3951f7154d1e4f9df69848
GuoWenhao1996 <1842297753@qq.com>
2016-12-07 00:00:03 +0800

    上传选课管理与成绩管理的查询功能

--- Student_java_sql/Panel_xscjgl.java
+++ Student_java_sql/Panel_xscjgl.java
@@ -1,14 +1,21 @@
 
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Vector;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableColumn;
 
 /*
@@ -22,54 +29,185 @@ import javax.swing.table.TableColumn;
  */
 public class Panel_xscjgl extends JPanel {
 
-//    private JTable table = null;
-//    private JScrollPane scrollpane = null;
-//    private Vector rowData = new Vector();
-//    private Vector columName = new Vector();
-//
+    private JTable table = new JTable();
+    private JScrollPane scrollpane = null;
+    private Vector rowData = new Vector();
+    private Vector columName = new Vector();
+
+    private JButton button_chaxun_xh = new JButton("查询");
+    private JLabel lable_xh = new JLabel("学号");
+    private JTextField textfield_xh = new JTextField(10);
+    private JButton button_chaxun_kch = new JButton("查询");
+    private JLabel lable_kch = new JLabel("      课程号");
+    private JTextField textfield_kch = new JTextField(10);
+    private JButton button_chaxun_xm = new JButton("查询");
+    private JLabel lable_xm = new JLabel("姓名");
+    private JTextField textfield_xm = new JTextField(10);
+    private JButton button_chaxun_kcm = new JButton("查询");
+    private JLabel lable_kcm = new JLabel("      课程名");
+    private JTextField textfield_kcm = new JTextField(10);
+
     protected JPanel p = new JPanel();
-//
-//    protected Panel_xscjgl() {
-//        Information();
-//        BoxLayout horizontal = new BoxLayout(p, BoxLayout.Y_AXIS);
-//        p.setLayout(horizontal);
-//        p.add(scrollpane);
-//    }
-//
-//    private void Information() {
-//        Connection dbConn = null;
-//        Statement dbState = null;
-//        ResultSet dbRs = null;
-//        String sql = null;
-//        DBHelper dbhelpr = new DBHelper();
-//        columName.add("111");
-//        columName.add("222");
-//
-//        //查询学生信息管理表
-//        try {
-//            dbConn = dbhelpr.GetConnection();
-//            dbState = dbConn.createStatement();
-//            sql = "";
-//            dbRs = dbState.executeQuery(sql);
-//            while (dbRs.next()) {
-//                Vector vNext = new Vector();
-//                vNext.add(dbRs.getString("xh"));
-//                vNext.add(dbRs.getString("xsxm"));
-//                rowData.add(vNext);
-//            }
-//            table = new JTable(rowData, columName);//数据加到表格中
-//            dbRs.close();
-//            dbState.close();
-//            dbhelpr.Close();
-//        } catch (SQLException ex) {
-//            System.err.println(ex.getMessage());
-//        }
-//        TableColumn column = null;
-//        column = table.getColumnModel().getColumn(0);
-//        column.setPreferredWidth(300);
-//
-//        table.setPreferredScrollableViewportSize(new Dimension(1000, 350));
-//        table.setAutoResizeMode(JTable.AUTO_RESIZE_NEXT_COLUMN);
-//        scrollpane = new JScrollPane(table);
-//    }
+    private JPanel p1 = new JPanel();
+    private JPanel p2 = new JPanel();
+    private JPanel p3 = new JPanel();
+
+    protected Panel_xscjgl() {
+        Information("select a.xh,xsxm,a.kcdm,kcmc,cj,bkcj "
+                + "from xk a,xs b,kc c "
+                + "where a.xh=b.xh and a.kcdm=c.kcdm");
+        myEventListener();
+        BoxLayout horizontal = new BoxLayout(p, BoxLayout.Y_AXIS);
+        p.setLayout(horizontal);
+        p1.add(lable_xh);
+        p1.add(textfield_xh);
+        p1.add(button_chaxun_xh);
+        p1.add(lable_kch);
+        p1.add(textfield_kch);
+        p1.add(button_chaxun_kch);
+        p2.add(lable_xm);
+        p2.add(textfield_xm);
+        p2.add(button_chaxun_xm);
+        p2.add(lable_kcm);
+        p2.add(textfield_kcm);
+        p2.add(button_chaxun_kcm);
+        p3.add(scrollpane);
+        p.add(p1);
+        p.add(p2);
+        p.add(p3);
+    }
+
+    private void Information(String sql) {
+        Connection dbConn = null;
+        Statement dbState = null;
+        ResultSet dbRs = null;
+        DBHelper dbhelpr = new DBHelper();
+        rowData.clear();
+        columName.add("学号");
+        columName.add("姓名");
+        columName.add("课程号");
+        columName.add("课程名");
+        columName.add("成绩");
+        columName.add("补考成绩");
+
+        //查询学生选课表
+        try {
+            dbConn = dbhelpr.GetConnection();
+            dbState = dbConn.createStatement();
+            dbRs = dbState.executeQuery(sql);
+            while (dbRs.next()) {
+                Vector vNext = new Vector();
+                vNext.add(dbRs.getString("xh"));
+                vNext.add(dbRs.getString("xsxm"));
+                vNext.add(dbRs.getString("kcdm"));
+                vNext.add(dbRs.getString("kcmc"));
+                vNext.add(dbRs.getString("cj"));
+                vNext.add(dbRs.getString("bkcj"));
+                rowData.add(vNext);
+            }
+            table = new JTable(rowData, columName);//数据加到表格中
+            dbRs.close();
+            dbState.close();
+            dbhelpr.Close();
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+        table.setFont(new Font("Dialog", 0, 15));
+
+        TableColumn column = null;
+
+        column = table.getColumnModel().getColumn(3);
+        column.setPreferredWidth(300);
+        column = table.getColumnModel().getColumn(4);
+        column.setPreferredWidth(50);
+        column = table.getColumnModel().getColumn(5);
+        column.setPreferredWidth(50);
+
+        DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
+        renderer.setHorizontalAlignment(DefaultTableCellRenderer.CENTER);
+        table.setDefaultRenderer(Object.class, renderer);
+
+        table.setPreferredScrollableViewportSize(new Dimension(850, 350));
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_NEXT_COLUMN);
+        scrollpane = new JScrollPane(table);
+    }
+
+    private void myEventListener() {
+        //学号查询事件监听
+        button_chaxun_xh.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textfield_kch.setText("");
+                textfield_kcm.setText("");
+                textfield_xm.setText("");
+                if (textfield_xh.getText().equals("") || textfield_xh.getText().equals("学号不能为空！")) {
+                    textfield_xh.setText("学号不能为空！");
+                    MainFrame.mf.repaint();
+                } else {
+                    String _XH = textfield_xh.getText();
+                    Information("select a.xh,xsxm,a.kcdm,kcmc,cj,bkcj "
+                            + "from xk a,xs b,kc c "
+                            + "where a.xh=b.xh and a.kcdm=c.kcdm and a.xh='" + _XH + "'");
+                    MainFrame.mf.repaint();
+                }
+            }
+        });
+        //课程号查询事件监听
+        button_chaxun_kch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textfield_xh.setText("");
+                textfield_kcm.setText("");
+                textfield_xm.setText("");
+                if (textfield_kch.getText().equals("") || textfield_kch.getText().equals("课程号不能为空！")) {
+                    textfield_kch.setText("课程号不能为空！");
+                    MainFrame.mf.repaint();
+                } else {
+                    String _KCH = textfield_kch.getText();
+                    Information("select a.xh,xsxm,a.kcdm,kcmc,cj,bkcj "
+                            + "from xk a,xs b,kc c "
+                            + "where a.xh=b.xh and a.kcdm=c.kcdm and a.kcdm='" + _KCH + "'");
+                    MainFrame.mf.repaint();
+                }
+            }
+        });
+        //姓名查询事件监听
+        button_chaxun_xm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textfield_xh.setText("");
+                textfield_kcm.setText("");
+                textfield_kch.setText("");
+                if (textfield_xm.getText().equals("") || textfield_xm.getText().equals("姓名不能为空！")) {
+                    textfield_xm.setText("姓名不能为空！");
+                    MainFrame.mf.repaint();
+                } else {
+                    String _XM = textfield_xm.getText();
+                    Information("select a.xh,xsxm,a.kcdm,kcmc,cj,bkcj "
+                            + "from xk a,xs b,kc c "
+                            + "where a.xh=b.xh and a.kcdm=c.kcdm and xsxm='" + _XM + "'");
+                    MainFrame.mf.repaint();
+                }
+            }
+        });
+        //课程名查询事件监听
+        button_chaxun_kcm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textfield_xh.setText("");
+                textfield_kch.setText("");
+                textfield_xm.setText("");
+                if (textfield_kcm.getText().equals("") || textfield_kcm.getText().equals("课程名不能为空！")) {
+                    textfield_kcm.setText("课程名不能为空！");
+                    MainFrame.mf.repaint();
+                } else {
+                    String _KCM = textfield_kcm.getText();
+                    Information("select a.xh,xsxm,a.kcdm,kcmc,cj,bkcj "
+                            + "from xk a,xs b,kc c "
+                            + "where a.xh=b.xh and a.kcdm=c.kcdm and kcmc like'%" + _KCM + "%'");
+                    MainFrame.mf.repaint();
+                }
+            }
+        });
+    }
 }
